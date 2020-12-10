6abc79ec50dc8d1bf2c984e4d4b08a80730aecb2
hain9563 <32196586+hain9563@users.noreply.github.com>
2020-12-10 15:58:57 +0900

    (If applied, this commit will) create selfRotate method
    면이 돌아갈 때 자기 자신이 시계방향으로 90도 돌아가는 기능을 구현한 메소드이다.
    주변의 다른 면이 돌아가는 기능은 각 면마다 하나씩 구현할 예정이다.

--- codesquad_2021/src/Rubikscube.java
+++ codesquad_2021/src/Rubikscube.java
@@ -1,5 +1,7 @@
+// 면 하나가 회전 = 자기 자신도 돌아감(rotate_self) + 주변의 면도 같이 돌아감(각 면마다 다르게? 해야할 듯)
 
 public class Rubikscube {
+		static char[][] cube;
 		static final char BLACK = 'B';
 		static final char RED = 'R';
 		static final char WHITE = 'W';
@@ -7,36 +9,67 @@ public class Rubikscube {
 		static final char GREEN = 'G';
 		static final char YELLOW = 'Y';
 		static final char EMPTY  = '\u0000';
- 	public static char[][] init(){ 
-	 		char[][] cube = { {EMPTY, EMPTY, EMPTY, BLACK, BLACK, BLACK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}, 
-	 						{EMPTY, EMPTY, EMPTY, BLACK, BLACK, BLACK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}, 
-	 						{EMPTY, EMPTY, EMPTY, BLACK, BLACK, BLACK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}, 
-     						{WHITE, WHITE, WHITE, ORANGE, ORANGE, ORANGE, GREEN, GREEN, GREEN, YELLOW, YELLOW, YELLOW}, 
-	 						{WHITE, WHITE, WHITE, ORANGE, ORANGE, ORANGE, GREEN, GREEN, GREEN, YELLOW, YELLOW, YELLOW}, 
-	 						{WHITE, WHITE, WHITE, ORANGE, ORANGE, ORANGE, GREEN, GREEN, GREEN, YELLOW, YELLOW, YELLOW}, 
-	 						{EMPTY, EMPTY, EMPTY, RED, RED, RED, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}, 
-	 						{EMPTY, EMPTY, EMPTY, RED, RED, RED, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}, 
-	 						{EMPTY, EMPTY, EMPTY, RED, RED, RED, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}}; 
-	 		return cube; 
-	 	} 
-
-	
-	//전개도 출력 함수
-	static void printCube(char[][] arr) {
-		int i, j;
-		for (i = 0; i < arr.length; i++) {
-			for (j = 0; j < arr[i].length; j++) {
-				System.out.print(arr[i][j] + " ");
-			}
-			System.out.println("");
+		
+		
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			cube = init();
+			printCube(cube);
+//			selfRotate(3,6,9,12);
+//			System.out.println("\n");
+//			printCube(cube);
+			
+			
+			
 		}
-	}
+		
+		
+		
+	 	public static char[][] init(){ 
+		 		char[][] cube = { {EMPTY, EMPTY, EMPTY, BLACK, BLACK, BLACK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}, 
+		 						{EMPTY, EMPTY, EMPTY, BLACK, BLACK, BLACK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}, 
+		 						{EMPTY, EMPTY, EMPTY, BLACK, BLACK, BLACK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}, 
+	     						{WHITE, WHITE, WHITE, ORANGE, ORANGE, ORANGE, GREEN, GREEN, GREEN, '1', '2', '3'}, 
+		 						{WHITE, WHITE, WHITE, ORANGE, ORANGE, ORANGE, GREEN, GREEN, GREEN, '4', '5', '6'}, 
+		 						{WHITE, WHITE, WHITE, ORANGE, ORANGE, ORANGE, GREEN, GREEN, GREEN, '7', '8', '9'}, 
+		 						{EMPTY, EMPTY, EMPTY, RED, RED, RED, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}, 
+		 						{EMPTY, EMPTY, EMPTY, RED, RED, RED, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}, 
+		 						{EMPTY, EMPTY, EMPTY, RED, RED, RED, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY}}; 
+		 		return cube; 
+		 	} 
+	 	
+	 
+		
+		//전개도 출력 함수
+		static void printCube(char[][] arr) {
+			int i, j;
+			for (i = 0; i < arr.length; i++) {
+				for (j = 0; j < arr[i].length; j++) {
+					System.out.print(arr[i][j] + " ");
+				}
+				System.out.println("");
+			}
+		}
+		
+		//돌릴 면 하나 자기 자신만 돌리기 : 면을 복사한 후에 시계방향으로 돈 결과를 cube에 다시 반영해줌
+		public static void selfRotate(int start_row, int end_row, int start_column, int end_column) {
+			char [][] copy_cube = new char[3][3];
+			for(int i=start_row;i<end_row;i++) {
+				for(int j=start_column;j<end_column;j++) {
+					copy_cube[i-start_row][j-start_column] = cube[i][j];
+				}
+			}
+			
+			for(int i=0; i<copy_cube[0].length; i++){
+		        for(int j=copy_cube.length-1; j>=0; j--){
+		            cube[start_row+i][end_column-j-1] = copy_cube[j][i];
+		        }
+		    }
+			
+			
+			
+		}
 	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		char[][] cube = init();
-		printCube(cube);
-	}
+
 
 }
